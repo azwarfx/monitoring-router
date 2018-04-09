71e11c9369278591b112e83ff1543b8ce22437ac
azwarfx <azwarannas_95@yahoo.com>
2018-04-09 23:51:47 +0700

    build 2

--- MonitoringRouter/src/monitoringrouter/MainApp.java
+++ MonitoringRouter/src/monitoringrouter/MainApp.java
@@ -5,6 +5,8 @@
  */
 package monitoringrouter;
 
+import monitoringrouter.view.Ping;
+
 /**
  *
  * @author azwarazuhri
@@ -16,6 +18,8 @@ public class MainApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Ping buka = new Ping();
+        buka.setVisible(true);
     }
     
 }
